081e0a27b195b3a174acb8b2b4b6686b8c2fe689
Milan <35422302+milanvarghese@users.noreply.github.com>
2019-09-26 14:41:46 +0530

    updation

--- Core python programming/Networking in Python/Server1.py
+++ Core python programming/Networking in Python/Server1.py
@@ -1,31 +1,31 @@
 #A python program to create a TCP/IP server program that sends messafes to a client
-
-#importing Modules
 import socket
-
 #takes server name and port number
 host='localhost'
 port=5000
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM) #Create a socket at server side using TCP/IP protocol 
+s.bind((host,port)) #Bind the socket with server and port number
+s.listen(1) #allow minimum 1 connetion in the socket
+c, addr =s.accept() #wait till client accepts connection
+print("Connection from: ",str(addr)) #Display client address
 
-#Create a socket at server side using TCP/IP protocol 
-s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-#Bind the socket with server and port number
-s.bind((host,port))
+m1,m2='0','0'
+print("Type 'Exit' to exit the chat")
+while 1:
+    m1=input("Enter Message: ")
+    if m1=='Exit' or m1=='exit':
+        break
+    elif m1=='0':
+        continue
+    else:
+        c.send(m1.encode())
+        m1='0'
+    m2=c.recv(1024)
+    m2.decode()
+    if m2!='0':
+        print("Client: "+m2)
+        m2='0'
 
-#allow minimum 1 connetion in the socket
-s.listen(1)
-
-#wait till client accepts connection
-c, addr =s.accept()
-
-#Display client address
-print("Connection from: ",str(addr))
-
-#Send messages to client after encoding it intobinary string
-c.send(b"Hello client, How are you")
-msg="Bye!"
-c.send(msg.encode())
-
-#disconnect the server
-c.close()
+if m1=='Exit' or m1=='exit':
+    c.close() 
